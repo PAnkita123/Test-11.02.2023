8b68e47ed7a03f7a4f66c50a943bba726657edc6
PAnkita123 <malviyaankita901@gmail.com>
2023-02-14 10:09:07 +0530

    commit

---
+++ java.js
@@ -0,0 +1,10 @@
+// alert("Hello Ankita! Welcome back, Happy Shopping😊");
+
+// // var tweet=prompt("write your complain:");
+// // var tweetCount=tweet.length;
+// // alert("you have written"+ tweetCount +"characters, you have"+(140-tweetCount)+" characters reamining,");
+
+// showMessage("Hello");
+
+let welcome="Welcome!"
+showMessage(welcome);
