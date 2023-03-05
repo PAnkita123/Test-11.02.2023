5d6297e852c5694e7ada25c81aba0ecd93a72a66
PAnkita123 <malviyaankita901@gmail.com>
2023-03-05 07:10:06 +0530

    commit

--- script.js
+++ script.js
@@ -36,7 +36,77 @@ function register(event) {
       document.getElementById("userNumber").value = '';
       document.getElementById("userEmail").value = '';
       document.getElementById("userPassword").value = '';
-      window.location.href = './login.html';
+      window.location.href = './signin.html';
       alert('Registration Done.');
   }
+}
+
+function login(event) {
+    event.preventDefault();
+    var userEmail = document.getElementById("email").value;
+    var userPassword = document.getElementById("password").value;
+
+    var dataFromLS = JSON.parse(localStorage.getItem('userData'));
+
+    var flag = false;
+
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword)
+            flag = true;
+
+    }
+    if (flag === true) {
+        document.getElementById("email").value = '';
+        document.getElementById("password").value = '';
+        alert("You are logged in successfully!");
+        window.location.href = "./index.html";
+        
+    } 
+    else {
+        alert("Wrong credentials entered!Please use correct email and password!");
+    }
+
+}
+
+var gettingEmail;
+function forgetPassword(){
+    // alert("worked!")
+    var dataFromLS=JSON.parse(localStorage.getItem("userData"));
+    var userEmail=document.getElementById("email").value;
+    gettingEmail=userEmail;
+
+    var flag=false;
+    for(var i=0; i<dataFromLS.length; i++){
+        if (dataFromLS[i].email===userEmail){
+            flag=true;
+        }
+
+    }
+    if(flag===true){
+        var newCode=`<div id=set-pass><small>Set a New Password!</small><br/><input type="password" id="password"/><br/><br/><button onclick="newPassword()">set new password</button></div>`
+        var divFromHtml= document.getElementById("change")
+        divFromHtml.innerHTML=newCode;
+        // window.location.href="./new-pass.html";
+        alert("Now set new password")
+    }else{
+        alert("please check your email, its not in our database!")
+    }
+}
+
+
+function newPassword(){
+    // alert("worked!")
+    var userPassword=document.getElementById("password").value;
+    var dataFromLS=JSON.parse(localStorage.getItem("userData"));
+
+    for(var i=0; i<dataFromLS.length;i++){
+        if(dataFromLS[i].email===gettingEmail){
+            dataFromLS[i].password=userPassword
+        }
+    }
+    localStorage.setItem('userData', JSON.stringify(dataFromLS));
+    gettingEmail="";
+    window.location.href='./login.html';
+    alert("password changed, now login.")
+
 }
